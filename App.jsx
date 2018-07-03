9f91f709a95fb758e9773dbba77a2ae8128f2354
Michael Allen <mike@mistColoured.com>
2018-07-03 15:31:56 +0100

    Housekeeping

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 
 import Toggle from './RenderProps/ToggleChildren';
+import Portal from './RenderProps/Portal';
 
 const App = () => (
   <div>
@@ -9,9 +10,13 @@ const App = () => (
         <Fragment>
           {on && <h1>Shown Me</h1>}
           <button onClick={toggle}>Show/Hidden</button>
+          <Portal>
+            {on && <h1>Hi, I am a Portal</h1>}
+          </Portal>
         </Fragment>
       )}
     </Toggle>
+
   </div>
 );
 
--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Portal from './RenderProps/Portal';
 
 ReactDOM.render(<App />, document.getElementById('app'));
